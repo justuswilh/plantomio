2f6ff261a2245987dd54b43e3a961e97895b6393
Dev-Linus <work@linus-schneider.com>
2024-07-27 23:36:19 +0200

    implemented AP behavior for Olimex
    -Olimex starts an Access Point and accepts whitelisted devices only

---
+++ access_point.py
@@ -0,0 +1,75 @@
+import os
+import subprocess
+import random
+import string
+import uuid
+
+ssid = "Plantomio Controller"
+whitelist = ["5c:85:7e:b0:7d:33","5c:85:7e:b0:c2:dd"] #flowercare devices whitelisted
+network_i="wlan0"
+
+def create_hostapd_conf(ssid, password):
+    hostapd_conf = f"""
+interface={network_i}
+driver=nl80211
+ssid={ssid}
+hw_mode=g
+channel=6
+wmm_enabled=0
+macaddr_acl=1
+accept_mac_file=/etc/hostapd/hostapd.accept
+auth_algs=1
+ignore_broadcast_ssid=0
+wpa=2
+wpa_passphrase={password}
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP
+rsn_pairwise=CCMP
+    """
+    with open('/etc/hostapd/hostapd.conf', 'w') as file:
+        file.write(hostapd_conf)
+    
+def create_dnsmasq_conf():
+    dhcp_host_conf = ""
+    for i,mac in enumerate(whitelist):
+        if i < 98:
+            dhcp_host_conf = dhcp_host_conf+"\ndhcp-host="+ mac + ",192.168.150."+ str(i+2)
+
+    dnsmasq_conf = f"""
+interface={network_i}
+dhcp-range=192.168.150.2,192.168.150.30,255.255.255.0,24h
+    """
+
+    print(dnsmasq_conf+dhcp_host_conf)
+    with open('/etc/dnsmasq.conf', 'w') as file:
+        file.write(dnsmasq_conf+dhcp_host_conf)
+
+def configure_network(network_interface):
+    # Set up the network interface
+    subprocess.run(['ifconfig', network_interface, 'up'], check=True)
+    subprocess.run(['ifconfig', network_interface, '192.168.150.1'], check=True)
+    subprocess.run(['systemctl', 'restart', 'hostapd'], check=True)
+    subprocess.run(['systemctl', 'restart', 'dnsmasq'], check=True)
+
+
+def create_mac_file (whitelist):
+    os.makedirs("/etc/hostapd/", exist_ok=True)
+
+    with open("/etc/hostapd/hostapd.accept", 'w') as file:
+        file.write(whitelist)
+
+def start_access_point():   #reqirements: sudo apt-get install hostapd dnsmasq
+
+    # own Mac-Address is used as seed for wifi-password
+    mac = uuid.UUID(int=uuid.getnode()).hex[-12:]
+    formatted_mac = ":".join(mac[i:i+2] for i in range(0, 12, 2))
+    random.seed(formatted_mac)
+    password = "".join(random.choices(string.printable, k=25))
+    
+    print(password)
+
+    create_mac_file(whitelist) #only works on linux
+    create_hostapd_conf(ssid, password) #only works on linux
+    create_dnsmasq_conf()
+    configure_network() #only works on linux
+    print(f'Access Point {ssid} wurde erstellt.')
